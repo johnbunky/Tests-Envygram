8dff36df97c7c3f84558876f7ff0fa151c897369
johnbunky <johnbunky@mail.ru>
2016-10-07 20:10:54 +0300

    Code must be better

--- src/test/java/org/selenide/examples/CreateANewPost.java
+++ src/test/java/org/selenide/examples/CreateANewPost.java
@@ -2,8 +2,9 @@ package org.selenide.examples;
 
 import org.testng.annotations.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byTitle;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -16,28 +17,37 @@ public class CreateANewPost {
     public void createAPost_ImgFromComp_succes(){
         //arrange
         open("http://68.169.52.12/EnvyGram/html/");
-        $("#loginDialogButton").click();
-        $("#UserLogin_email").setValue("John");
-        $("#UserLogin_password").setValue("bn3211").pressEnter();
-        $(".badge.createPost").click();
-        $("#yw14>li>a").click();
+        login("John", "bn3211");
+        openCreateNewPostWindow();
         //act
-        $("#PostCreate_header").setValue("Headline");
-        $("#PostCreate_description").setValue("Description");
-        $("#PostCreate_adType").click();
-                 // как выбрать из выпадающего меню??????
-                 // допустим выбрал default
-        $(".span10.dropdown-toggle.mediaDropdown.btn").shouldHave(text("Image From Computer")); // check a source
-        $(".fileField.inputFile").click();
-                 // Как выбрать фото из окна не в браузере????
-                 // choose photo, click open
-        // assert
-        $(".fileField.inputFile").shouldHave(text("File selected"));
-        $(".create-label").click();
-                  //как подождать када стр загрузиться???
+        fillRequiredFields("Headline", "Description", "Default", "IMG_20161006_123821.jpg");
+        //assert
+        Results();
+
+    }
+
+    private void Results() {
         $(byTitle("Headliner")).shouldHave(text("Headliner"));
         $(byTitle("Description")).shouldHave(text("Description"));
-        $(".attachment.view-right.text-center>img").shouldBe(visible); // годится такая проверка фото?
+        $(".attachment.view-right.text-center>img").isImage();
+    }
 
+    private void fillRequiredFields(String Headline, String Description, String AdType, String photo) {
+        $("#PostCreate_header").setValue(Headline);
+        $("#PostCreate_description").setValue(Description);
+        $("#PostCreate_adType").selectOption(AdType);
+        $(".fileField.inputFile").uploadFile( new File(photo));
+        $(".create-label").click();
+    }
+
+    private void openCreateNewPostWindow() {
+        $(".badge.createPost").click();
+        $("#yw14>li>a").click();
+    }
+
+    private void login(String name, String password) {
+        $("#loginDialogButton").click();
+        $("#UserLogin_email").setValue(name);
+        $("#UserLogin_password").setValue(password).pressEnter();
     }
 }
